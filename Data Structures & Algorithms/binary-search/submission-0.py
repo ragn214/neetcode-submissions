85dd5fb9ffe9d69dd0de7dd35a91bc890263b581
ragn214 <61681141+ragn214@users.noreply.github.com>
2026-06-06 22:25:10 +1200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = int((len(nums)-1)/2)
+        l = 0 # narrowing selection of the indexes
+        r = len(nums)-1 # narrowing selection of the indexes
+        while target != nums[index]:
+            if target < nums[index]:
+                r = index
+                new_index = int((r+l)/2)
+                if index == int(new_index):
+                    return -1
+                else:
+                    index = new_index
+                    
+            else: # if the target is greater than the new number
+                l = index + 1
+                new_index = int((r+l)/2)
+                if index == int(new_index):
+                    return -1
+                else:
+                    index = new_index
+                    
+            
+        return index
+
+        #[-1, 0, 2, 4, 6, 8, 9,10,11,12,13,14,15] numbers
+        #[ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12] index
+        #                      [0, 1, 2, 3, 4, 5]
+        # target = 12
+        #length = 13
+        #index = 6
+        #newindex = 
