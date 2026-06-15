a2c9c173b616e877b0dc02c71fe802bf6897f074
ragn214 <61681141+ragn214@users.noreply.github.com>
2026-06-15 21:31:07 +1200

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,16 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.words = []
+
+    def insert(self, word: str) -> None:
+        self.words.append(word)
+
+    def search(self, word: str) -> bool:
+        return word in self.words
+
+    def startsWith(self, prefix: str) -> bool:
+        for i in self.words:
+            if prefix == i[:len(prefix)]:
+                return True
+        return False
