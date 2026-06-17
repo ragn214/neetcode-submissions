a9fd13279553934d3febfbe3befce573aa9bb28f
ragn214 <61681141+ragn214@users.noreply.github.com>
2026-06-17 21:40:59 +1200

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # result = []
+        # current = []
+        result = [[] for _ in range(2**len(nums))]
+        for i in range(len(result)):
+            for j, num in enumerate(nums):
+                print(f"i = {i}, j = {j}") 
+                print((i / 2**(j))%2)
+                if (i // 2**(j)) % 2 == 0:
+
+                    result[i].append(nums[j])
+        return result
+        # for i in range(len(nums)):
+            
+
+
+        # i = 0
+        # while i < len(nums):
+        #     self.subsets(nums[i:])
+        #     i++
+        # if i
+        # return 0
